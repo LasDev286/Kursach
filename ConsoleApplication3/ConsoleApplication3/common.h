03b196c85b8c55e3b0a63494772fd21efd8e11cc
LasDev286 <moiseeva.milena@gmail.com>
2024-12-14 17:59:46 +0300

    1

---
+++ ConsoleApplication3/ConsoleApplication3/common.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#ifdef _WIN32 // If host's OS is Windows
+#ifndef _WIN32_WINNT // Necessary for properly initializing WindowsAPI
+#define _WIN32_WINNT 0x0600 // minimum - Windows Vista (0x0600)
+#endif
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib") // For VS compiler
+#else // Other UNIX-like OSes
+#include <sys/socket.h> 
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h> // For getting error codes
+#include <sys/select.h>
+#endif
+
+#include <string>
+#include <iostream>
+#include <cstring>
+#include <ctime>
+
+// Cross-Platform macros
+#ifdef _WIN32
+#define CLOSE_SOCKET(s) closesocket(s)
+#define GET_SOCKET_ERRNO() (WSAGetLastError())
+#define IS_VALID_SOCKET(s) ((s) != INVALID_SOCKET)
+#else
+#define SOCKET int
+#define CLOSE_SOCKET(s) close(s)
+#define GET_SOCKET_ERRNO() (errno)
+#define IS_VALID_SOCKET(s) ((s) >= 0)
+#endif
+
+#define MAX_DATA_BUFFER_SIZE 2048
+
+// Adds 4-digit length to the beginning of the message string: message.size() == 4 -> "0004<message_string>"; message.size() == 1002 -> "1002<message_string>"
+void PrependMessageLength(std::string& message) {
+    std::string message_size_str = std::to_string(message.size());
+    while (message_size_str.size() < 4) {
+        message_size_str = "0" + message_size_str;
+    }
+    message = message_size_str + message;
+}
